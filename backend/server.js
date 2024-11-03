1b3be2c5c664d530b5cbb591a36e5293b5e1dd10
Santiago Cabrera <santi.c.pgm.com>
2024-11-03 16:17:04 -0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,5 +11,5 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on porto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
