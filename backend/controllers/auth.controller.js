eec40fdc214919838e7fa36947fd3fc3468e3665
Santiago Cabrera <santi.c.pgm.com>
2024-11-04 10:24:47 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,14 @@
-import User from "../models/user.model";
+// import User from "../models/user.model";
 
-export const signup = async (req, res) => {
-  try {
-    const { name, username, email, password } = req.body;
-    const existingEmail = await User.findOne({ email });
-  } catch (error) {}
-};
-export const login = (req, res) => {
-  res.send("login");
-};
-export const logout = (req, res) => {
-  res.send("logout");
-};
+// export const signup = async (req, res) => {
+//   try {
+//     const { name, username, email, password } = req.body;
+//     const existingEmail = await User.findOne({ email });
+//   } catch (error) {}
+// };
+// export const login = (req, res) => {
+//   res.send("login");
+// };
+// export const logout = (req, res) => {
+//   res.send("logout");
+// };
