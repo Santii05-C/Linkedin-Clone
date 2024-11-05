3dcc22bb466164a3ff476a4cb437b8da2521dd88
Santiago Cabrera <santi.c.pgm.com>
2024-11-05 10:34:30 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,56 @@
-// import User from "../models/user.model";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-// export const signup = async (req, res) => {
-//   try {
-//     const { name, username, email, password } = req.body;
-//     const existingEmail = await User.findOne({ email });
-//   } catch (error) {}
-// };
-// export const login = (req, res) => {
-//   res.send("login");
-// };
-// export const logout = (req, res) => {
-//   res.send("logout");
-// };
+export const signup = async (req, res) => {
+  try {
+    const { name, username, email, password } = req.body;
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ msg: "Username already exists" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: "Password must be at least 6 characters" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      username,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("jtw-linkedin", token, {
+      httpOnly: true, //prevent XXS attack
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict", //prevent CSRF attacks,
+      secure: process.env.NODE_ENV === "production", //prevent man-in-the-middle attacks
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+
+    // todo: send welcome email
+  } catch (error) {}
+};
+export const login = (req, res) => {
+  res.send("login");
+};
+export const logout = (req, res) => {
+  res.send("logout");
+};
