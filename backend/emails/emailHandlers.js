ca62d82af70e80463e443e6bfc3e1b8e2ccd1e0b
Santii05-C <santi.c.pgm@gmail.com>
2024-11-08 10:35:05 -0300

    last commit

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -4,5 +4,16 @@ export const sendWelcomeEmail = async (email, name, profileUrl) => {
   const recipient = email[{ email }];
 
   try {
-  } catch (error) {}
+    const response = await mailtrapClient.send({
+      form: sender,
+      to: recipient,
+      subject: "Welcome to Unlinked",
+      html: createWelcomeEmailTemplate(name, profileUrl),
+      category: "Welcome",
+    });
+
+    console.log("Welcome Email sent successfully", response);
+  } catch (error) {
+    throw error;
+  }
 };
