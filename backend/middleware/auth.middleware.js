14c7988c4ff4389422d92006dbd4993cda66863d
Santii05-C <santi.c.pgm@gmail.com>
2024-11-08 22:16:15 -0300

    last commit

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,2 +1,18 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
+
+export const protectRoute = async (req, res) => {
+  try {
+    const token = req.cookie["jwt-linkdin"];
+
+    if (!token) {
+      return res.status(401).json({ msg: "Unathorized - No Token Provided " });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ msg: "Unathorized - Invalid Token" });
+    }
+  } catch (error) {}
+};
