b578fbd20d3d9b761e47c6128881deb31b3cce56
Santii05-C <santi.c.pgm@gmail.com>
2024-11-08 22:57:19 -0300

    last commit

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
 
-export const protectRoute = async (req, res) => {
+export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookie["jwt-linkdin"];
 
@@ -10,9 +10,21 @@ export const protectRoute = async (req, res) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     if (!decoded) {
       return res.status(401).json({ msg: "Unathorized - Invalid Token" });
     }
-  } catch (error) {}
+
+    const user = await User.findById(decoded.userId).select("password");
+
+    if (!user) {
+      return res.status(401).json({ msg: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute: middleware:", error.message);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
 };
