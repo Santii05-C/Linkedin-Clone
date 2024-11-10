6af4b63f3c53410d9b343c0c7a4b359f6db74e34
Santii05-C <santi.c.pgm@gmail.com>
2024-11-10 10:22:07 -0300

    last commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,23 +1,40 @@
-// import User from "../models/user.model.js";
+import User from "../models/user.model.js";
 
-// export const getSuggestedConnections = async (req, res) => {
-//   try {
-//     const currentUser = await User.dindById(req.user._id).select("connections");
+export const getSuggestedConnections = async (req, res) => {
+  try {
+    const currentUser = await User.dindById(req.user._id).select("connections");
 
-//find users who are not already connected, and also recommend our own profile right?
+    //find users who are not already connected, and also recommend our own profile right?
 
-//     const suggestedUsers = await User.find({
-//       _id: {
-//         $ne: req.user._id,
-//         $nin: currentUser.connections,
-//       },
-//     })
-//       .select("name, username, profilePicture headline")
-//       .limit(3);
+    const suggestedUsers = await User.find({
+      _id: {
+        $ne: req.user._id,
+        $nin: currentUser.connections,
+      },
+    })
+      .select("name, username, profilePicture headline")
+      .limit(3);
 
-//     res.json(suggestedUsers);
-//   } catch (error) {
-//     console.error("Error in getSuggestedConnections controller:", error);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+    res.json(suggestedUsers);
+  } catch (error) {
+    console.error("Error in getSuggestedConnections controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getPublicProfile = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password"
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error in getPublicProfile controller:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getSuggestedConnections } from "../controllers/user.controller.js";
+import {
+  getSuggestedConnections,
+  getPublicProfile,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/seggestions", protectRoute, getSuggestedConnections);
+router.get("/:username", protectRoute, getPublicProfile);
 
 export default router;
