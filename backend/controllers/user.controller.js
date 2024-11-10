a3ec7c15146d038df8209c37965e9cbdebcacd2c
Santii05-C <santi.c.pgm@gmail.com>
2024-11-09 22:13:47 -0300

    last commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,22 +1,23 @@
-import User from "../models/user.model.js";
+// import User from "../models/user.model.js";
 
-export const getSuggestedConnections = async (req, res) => {
-  try {
-    const currentUser = await User.dindById(req.user._id).select("connections");
+// export const getSuggestedConnections = async (req, res) => {
+//   try {
+//     const currentUser = await User.dindById(req.user._id).select("connections");
 
-    //find users who are not already connected, and also recommend our own profile right?
-    const suggestedUsers = await User.find({
-      _id: {
-        $ne: req.user._id,
-        $nin: currentUser.connections,
-      },
-    })
-      .select("name, username, profilePicture headline")
-      .limit(3);
+//find users who are not already connected, and also recommend our own profile right?
 
-    res.json(suggestedUsers);
-  } catch (error) {
-    console.error("Error in getSuggestedConnections controller:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
+//     const suggestedUsers = await User.find({
+//       _id: {
+//         $ne: req.user._id,
+//         $nin: currentUser.connections,
+//       },
+//     })
+//       .select("name, username, profilePicture headline")
+//       .limit(3);
+
+//     res.json(suggestedUsers);
+//   } catch (error) {
+//     console.error("Error in getSuggestedConnections controller:", error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
