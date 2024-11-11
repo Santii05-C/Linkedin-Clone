9499c52e4d593273afea3d6d3b590d6125655f6e
Santii05-C <santi.c.pgm@gmail.com>
2024-11-11 20:55:16 -0300

    last commit

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
+
+router.get("/", protectRoute, getFeedPosts);
 
 export default router;
