05003c112ff8a90fe6772c74314c3c051dedcf44
Santii05-C <santi.c.pgm@gmail.com>
2024-11-13 21:07:56 -0300

    last commit

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -13,3 +13,19 @@ export const getNotifications = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const markNotificationRead = async (req, res) => {
+  const notificatiionId = req.params.id;
+  try {
+    const notificatiion = await Notification.findByIdAndUpdate(
+      { _id: notificatiionId, recipient: req.user._id },
+      { read: true },
+      { new: true }
+    );
+
+    res.json(notificatiion);
+  } catch (error) {
+    console.error("Error in markNotificationAsRead controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
