b7ae31bcd183a5c3bdb67ce420d0bc5ed5a92bd5
Santii05-C <santi.c.pgm@gmail.com>
2024-11-15 15:00:33 -0300

    last commit

---
+++ backend/controllers/connection.controller.js
@@ -0,0 +1,81 @@
+import ConnectionRequest from "../models/connectionRequest.model.js";
+
+export const sendConnectionRequest = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const senderId = req.user._id;
+
+    if (senderId.toString() === userId) {
+      return res
+        .status(400)
+        .json({ message: "You can't send a request to yourself" });
+    }
+
+    if (req.user.connections.includes(userId)) {
+      return res.status(400).json({ message: "You are already connected" });
+    }
+
+    const existingRequest = await ConnectionRequest.findOne({
+      sender: senderId,
+      recipient: userId,
+      status: "pending",
+    });
+
+    if (existingRequest) {
+      return res
+        .status(400)
+        .json({ message: "A connection request already exists" });
+    }
+
+    const newRequest = new ConnectionRequest({
+      sender: senderId,
+      recipient: userId,
+    });
+
+    await newRequest.save();
+
+    res.status(201).json({ message: "Connection request sent successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const acceptConnectionRequest = async (req, res) => {
+  try {
+    const { requestId } = req.params;
+    const userId = req.user._id;
+
+    const request = await ConnectionRequest.findById(requestId)
+      .populate("sender", "name email username")
+      .populate("recipient", "name username");
+
+    if (!request) {
+      return res.status(404).json({ message: "Connection request not found " });
+    }
+
+    // check if the req is for the current user
+    if (request.recipient._id.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to accept this request" });
+    }
+
+    if (request.status !== "pending") {
+      return res
+        .status(400)
+        .json({ message: "This request has already been processed" });
+    }
+
+    request.status = "accepted";
+    await request.save();
+
+    // if im your friend then ur also my friend ;)
+    await User.findByIdAndUpdate(request.sender._id, {
+      $addToSet: { connections: userId },
+    });
+    await User.findByIdAndUpdate(userId, {
+      $addToSet: { connections: request.sender._id },
+    });
+    await request.save();
+  } catch (error) {}
+};
