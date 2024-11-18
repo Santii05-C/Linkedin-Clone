2183facd223671d2b3791302dd62c9cfa3afa91b
Santii05-C <santi.c.pgm@gmail.com>
2024-11-18 17:11:55 -0300

    last commit

--- frontend/src/pages/auth/SignUpPage.jsx
+++ frontend/src/pages/auth/SignUpPage.jsx
@@ -1,5 +1,11 @@
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img className="mx-auto h-36 w-auto" src="/logo.svg" alt="LinkedIn" />
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
