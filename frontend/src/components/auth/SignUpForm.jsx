311832f9514343964634405d1f29d63484e85c1d
Santii05-C <santi.c.pgm@gmail.com>
2024-11-18 23:47:41 -0300

    last commit

---
+++ frontend/src/components/auth/SignUpForm.jsx
@@ -0,0 +1,11 @@
+const SignUpForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <div>SignUpForm</div>;
+};
+
+export default SignUpForm;
+//2:56
