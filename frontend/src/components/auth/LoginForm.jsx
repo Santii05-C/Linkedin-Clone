1af40cc03cce5aac79d4238924f0e68ef2707525
Santii05-C <santi.c.pgm@gmail.com>
2024-11-26 15:11:54 -0300

    lasr commit

--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { axiosInstance } from "../../lib/axios";
 import toast from "react-hot-toast";
+import { Loader } from "lucide-react";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
@@ -18,8 +19,35 @@ const LoginForm = () => {
     },
   });
 
-  return <div>LoginForm</div>;
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginMutation({ username, password });
+  };
 
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-md">
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+
+      <button type="submit" className="btn btn-primary w-full">
+        {isLoading ? <Loader className="size-5 animate-spin" /> : "Login"}
+      </button>
+    </form>
+  );
+};
 export default LoginForm;
 //3:33
