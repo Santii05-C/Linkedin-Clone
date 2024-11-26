3cfb28b8bd57de5aacbfc9b5cb4fa8610ae6ff12
Santii05-C <santi.c.pgm@gmail.com>
2024-11-26 10:59:04 -0300

    last commit

---
+++ frontend/src/components/auth/LoginForm.jsx
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { axiosInstance } from "../../lib/axios";
+import toast from "react-hot-toast";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const queryClient = useQueryClient();
+
+  const { mutate: loginMutation, isLoading } = useMutation({
+    mutationFn: (userData) => axiosInstance.post("/auth/login", userData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+    onError: (err) => {
+      toast.error(err.response.data.message || "Something went wrong");
+    },
+  });
+
+  return <div>LoginForm</div>;
+};
+
+export default LoginForm;
+//3:33
