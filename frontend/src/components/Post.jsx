4f592a5f70b72e2455dc6d3a37e7bb7d1a457b4e
Santii05-C <santi.c.pgm@gmail.com>
2024-11-29 22:46:42 -0300

    last commit

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,48 +1,48 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import { axiosInstance } from "../lib/axios";
-import toast from "react-hot-toast";
+// import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+// import { useState } from "react";
+// import { axiosInstance } from "../lib/axios";
+// import toast from "react-hot-toast";
 
-const Post = ({ post }) => {
-  const { data: authUser } = useQuery({ querykey: ["authUser"] });
-  const [showComments, setShowComments] = useState(false);
-  const [newComment, setNewComment] = useState("");
-  const [comments, setcomments] = useState(post.comments || []);
-  const isOwner = authUser._id === post.authUser._id;
-  const isLiked = post.likes.includes(authUser._id);
+// const Post = ({ post }) => {
+//   const { data: authUser } = useQuery({ querykey: ["authUser"] });
+//   const [showComments, setShowComments] = useState(false);
+//   const [newComment, setNewComment] = useState("");
+//   const [comments, setcomments] = useState(post.comments || []);
+//   const isOwner = authUser._id === post.authUser._id;
+//   const isLiked = post.likes.includes(authUser._id);
 
-  const queryClient = useQueryClient();
+//   const queryClient = useQueryClient();
 
-  const { mutate: deletePost, isPending: isDeletingPost } = useMutation({
-    mutationFn: async () => {
-      await axiosInstance.delete(`/posts/delete/${post._id}`);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["posts"] });
-      toast.success("Post deleted successfully");
-    },
-    onError: (error) => {
-      toast.error(error.message);
-    },
-  });
+//   const { mutate: deletePost, isPending: isDeletingPost } = useMutation({
+//     mutationFn: async () => {
+//       await axiosInstance.delete(`/posts/delete/${post._id}`);
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ["posts"] });
+//       toast.success("Post deleted successfully");
+//     },
+//     onError: (error) => {
+//       toast.error(error.message);
+//     },
+//   });
 
-  const { mutate: createComment, isPending: isCreatingComment } = useMutation({
-    mutationFn: async (newComment) => {
-      await axiosInstance.post(`/posts/${post._id}/comments`, {
-        content: newComment,
-      });
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ querykey: ["posts"] });
-      toast.success("Comments added successfully");
-    },
-    onError: (error) => {
-      toast.error(error.message);
-    },
-  });
-  //4:11
+//   const { mutate: createComment, isPending: isCreatingComment } = useMutation({
+//     mutationFn: async (newComment) => {
+//       await axiosInstance.post(`/posts/${post._id}/comments`, {
+//         content: newComment,
+//       });
+//     },
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ querykey: ["posts"] });
+//       toast.success("Comments added successfully");
+//     },
+//     onError: (error) => {
+//       toast.error(error.message);
+//     },
+//   });
+//   //4:11
 
-  return <div>Post</div>;
-};
+//   return <div>Post</div>;
+// };
 
-export default Post;
+// export default Post;
