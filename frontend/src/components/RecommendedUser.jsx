21e443845bbabf64d8c1057fbac182f14592c526
Santii05-C <santi.c.pgm@gmail.com>
2024-12-03 11:01:02 -0300

    last commit

--- frontend/src/components/RecommendedUser.jsx
+++ frontend/src/components/RecommendedUser.jsx
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const RecommendedUser = ({ user }) => {
   const queryClient = useQueryClient;
@@ -22,7 +23,58 @@ const RecommendedUser = ({ user }) => {
       toast.error(error.response?.data?.error || "An error occurred");
     },
   });
-  return <div>RecommendedUser</div>;
+
+  const { mutate: acceptReaquest } = useMutation({
+    mutationFn: (requestId) =>
+      axiosInstance.put(`/connections/accept/${requestId}`),
+    onSuccess: () => {
+      toast.success("Connection accepted successfully");
+      queryClient.invalidateQueries({
+        queryKey: ["connectionStatus", user._id],
+      });
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.error || "An error occurred");
+    },
+  });
+
+  const { mutate: rejectRequest } = useMutation({
+    mutationFn: (requestId) =>
+      axiosInstance.put(`/connections/reject/${requestId}`),
+    onSuccess: () => {
+      toast.success("Connection request rejected ");
+      queryClient.invalidateQueries({
+        queryKey: ["connectionStatus", user._id],
+      });
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.error || "An error occurred");
+    },
+  });
+
+  const renderButton = () => {
+    //4:40
+  };
+
+  return (
+    <div className="flex items-center justify-between mb-4">
+      <Link
+        to={`/profile/${user.username}`}
+        className="flex items-center flex-grow"
+      >
+        <img
+          src={user.profilePicture || "/avatar.png"}
+          alt={user.name}
+          className="w-12 h-12 rounded-full mr-3"
+        />
+        <div>
+          <h3 className="font-semibold text-sm">{user.name}</h3>
+          <p className="text-xs text-info">{user.headline}</p>
+        </div>
+      </Link>
+      {renderButton()}
+    </div>
+  );
 };
 
 export default RecommendedUser;
