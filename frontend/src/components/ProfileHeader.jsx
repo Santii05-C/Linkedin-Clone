96bd321e7e3fad4e3831e353e8dd38d126527b49
Santii05-C <santi.c.pgm@gmail.com>
2024-12-08 22:47:36 -0300

    last commit

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { axiosInstance } from "../lib/axios";
 import { toast } from "react-hot-toast";
 
+import { Camera, Clock, MapPin, UserCheck, UserPlus, X } from "lucide-react";
+
 const ProfileHeader = ({ userData, onSave, isOwnProfile }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedData, setEditedData] = useState({});
@@ -34,6 +36,74 @@ const ProfileHeader = ({ userData, onSave, isOwnProfile }) => {
       toast.error(error.response?.data?.message || "An error occurred");
     },
   });
+
+  const { mutate: acceptRequest } = useMutation({
+    mutationFn: (requestId) =>
+      axiosInstance.put(`/connections/accept/${requestId}`),
+    onSuccess: () => {
+      toast.success("Connection request accepted");
+      refetchConnectionStatus();
+      queryClient.invalidateQueries(["connectionRequests"]);
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.message || "An error occurred");
+    },
+  });
+
+  const { mutate: rejectRequest } = useMutation({
+    mutationFn: (requestId) =>
+      axiosInstance.put(`/connections/reject/${requestId}`),
+    onSuccess: () => {
+      toast.success("Connection request rejected");
+      refetchConnectionStatus();
+      queryClient.invalidateQueries(["connectionRequests"]);
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.message || "An error occurred");
+    },
+  });
+
+  const { mutate: removeConnection } = useMutation({
+    mutationFn: (userId) => axiosInstance.delete(`/connections/${userId}`),
+    onSuccess: () => {
+      toast.success("Connection removed");
+      refetchConnectionStatus();
+      queryClient.invalidateQueries(["connectionRequests"]);
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.message || "An error occurred");
+    },
+  });
+
+  const getConnectionStatus = useMemo(() => {
+    if (isConnected) return "connected";
+    if (!isConnected) return "not_connected";
+    return connectionStatus?.data?.status;
+  }, [isConnected, connectionStatus]);
+
+  const renderConnectionButton = () => {
+    const baseClass =
+      "text-white py-2 px-4 rounded-full transition duration-300 flex items-center justify-center";
+
+    switch (getConnectionStatus()) {
+      case "connected":
+        return (
+          <div className="flex gap-2 justify-center">
+            <div className={`${baseClass} bg-green-500 hover:bg-green-600`}>
+              <UserCheck size={20} className="mr-2" />
+              Connected
+            </div>
+            <button
+              className={`${baseClass} bg-red-500 hover:bg-red-600 text-sm`}
+              onClick={() => removeConnection(userData._id)}
+            >
+              <X size={20} className="mr-2" />
+              Remove Connection
+            </button>
+          </div>
+        );
+    }
+  };
 
   return <div>ProfileHeader</div>;
 };
