aefd64c59a451298c5aaabdef1975cfc572e6676
Santii05-C <santi.c.pgm@gmail.com>
2024-12-08 22:22:39 -0300

    last commit

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,4 +1,40 @@
-const ProfileHeader = () => {
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { axiosInstance } from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+const ProfileHeader = ({ userData, onSave, isOwnProfile }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedData, setEditedData] = useState({});
+  const queryClient = useQueryClient();
+
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
+  const { data: connectionStatus, refetch: refetchConnectionStatus } = useQuery(
+    {
+      queryKey: ["connectionStatus", userData._id],
+      queryFn: () => axiosInstance.get(`/connections/status/${userData._id}`),
+      enabled: !isOwnProfile,
+    }
+  );
+
+  const isConnected = userData.connections.some(
+    (connection) => connection === authUser._id
+  );
+
+  const { mutate: sendConnectionRequest } = useMutation({
+    mutationFn: (userId) =>
+      axiosInstance.post(`/connections/request/${userId}`),
+    onSuccess: () => {
+      toast.success("Connection request sent");
+      refetchConnectionStatus();
+      queryClient.invalidateQueries(["connectionRequests"]);
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.message || "An error occurred");
+    },
+  });
+
   return <div>ProfileHeader</div>;
 };
 
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import ProfileHeader from "../components/ProfileHeader";
@@ -39,6 +39,7 @@ const ProfilePage = () => {
   const handleSave = (updatedData) => {
     updateProfile(updatedData);
   };
+
   return (
     <div className="max-w-4xl mx-auto p-4">
       <ProfileHeader
