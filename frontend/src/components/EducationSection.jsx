98ee42833fc449d77ba28bd878ab39c6840886cf
Santii05-C <santi.c.pgm@gmail.com>
2024-12-09 22:42:00 -0300

    last commit

--- frontend/src/components/EducationSection.jsx
+++ frontend/src/components/EducationSection.jsx
@@ -1,4 +1,21 @@
-const EducationSection = () => {
+import { School, X } from "lucide-react";
+import { useState } from "react";
+const EducationSection = ({ userData, isOwnProfile, onSave }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [educations, setEducations] = useState(userData.education || []);
+  const [newEducation, setNewEducation] = useState({
+    school: "",
+    fieldOfStudy: "",
+    startYear: "",
+    endYear: "",
+  });
+
+  const handleAddEducation = () => {};
+
+  const handleDeleteEducation = () => {};
+
+  const handleSave = () => {};
+
   return <div>EducationSection</div>;
 };
 
